6680c3740f8a5dc73f2c098cdc245e95acdb2708
Fabien SAVY <fab.alchemy@gmail.com>
2019-04-29 23:07:52 +0200

    Debugged can_place_point + used global constants WIDTH & HEIGHT
    Is verification scalable?

--- GameEngine.py
+++ GameEngine.py
@@ -3,6 +3,8 @@ import numpy as np
 from functools import reduce
 
 # GAME CONSTANTS
+WIDTH = 7
+HEIGHT = 6
 VIDE = 0
 
 class GameEngine:
@@ -14,16 +16,17 @@ class GameEngine:
         self.currentPlayer = player1
 
     def can_place_point(self, i):
-        if self.grid[i][0] == VIDE:
-            return True
+        if i >= 0 and i < WIDTH:
+            for j in reversed(range(HEIGHT)):
+                if self.grid[i][j] == VIDE:
+                    return True
         return False
-        
+
     def nextPlayer(self):
         self.currentPlayer = (self.player1, self.player2)[self.currentPlayer == self.player1]
-        
+
     def place_jeton(self, i):
-        print(self.grid[2][2])
-        for j in reversed(range(6)):
+        for j in reversed(range(HEIGHT)):
             if self.grid[i][j] == VIDE:
                 self.grid[i][j] = self.currentPlayer
                 return
@@ -31,61 +34,54 @@ class GameEngine:
 
     def gameIsOver(self):
         print('winner is : ', self.currentPlayer)
-        
+
     def checkIfWin(self, L):
         for i in range(0,len(L) - 3):
             if reduce(lambda a,b: a&b, L[i:i+4]) != 0:
                 self.gameIsOver()
-                   
+
     def get10value(self, n):
         return int(n == self.currentPlayer)
-        
+
     def get10grid(self):
-        newGrid = np.zeros((7,6), dtype=int)
-        for j in range(6):
-            for i in range(7):
+        newGrid = np.zeros((WIDTH,HEIGHT), dtype=int)
+        for j in range(HEIGHT):
+            for i in range(WIDTH):
                 newGrid[i][j] = self.get10value(self.grid[i][j])
         return newGrid
-        
+
     def verifIfThereIsAWinner(self):
         grid2 = self.get10grid()
         L1 = []
-        for i in range(7):
+        for i in range(WIDTH):
             L1.append(int(''.join(map(str, grid2[i])), 2))
         L2 = []
-        for j in range(6):
+        for j in range(HEIGHT):
             L2.append(int(''.join(map(str, grid2[:,j])), 2))
 
         L3 = [L1[i] << i for i in range(len(L1))]
-        L4 = [L1[6-i] << i for i in range(len(L1))]
-                
+        L4 = [L1[HEIGHT-i] << i for i in range(len(L1))]
+
         self.checkIfWin(L1)
         self.checkIfWin(L2)
         self.checkIfWin(L3)
         self.checkIfWin(L4)
-        
+
         self.nextPlayer()
-        
+
     def printGrid(self):
-        for j in range(6):
-            for i in range(7):
+        for j in range(HEIGHT):
+            for i in range(WIDTH):
                 print (self.grid[i][j], end=' ')
             print()
-            
+
 
 Game = GameEngine(2,1)
 while True:
     n = int(input())
-    Game.place_jeton(n)
+    if Game.can_place_point(n):
+        Game.place_jeton(n)
+    else:
+        print("Impossible de jouer dans la colonne {}".format(n))
     Game.printGrid()
     Game.verifIfThereIsAWinner()
-
-
-
-
-
-
-
-
-
-
